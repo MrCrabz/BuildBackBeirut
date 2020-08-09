4037074aa09313ded9877fe87ceb45d47e619d8a
Robin Geagea <Robin.geagea.rg@gmail.com>
2020-08-09 22:45:09 +0300

    map update

---
+++ map/mapbox.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW5nZWFnZWEiLCJhIjoiY2tkbmZ5NnN6MWhnMDJzcm9sOW15NmZlbSJ9.yjhyjQzprYOcVmB4UMri5A';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/robingeagea/ckdlc70gv1z5z1imwitqyd11s',
+  center: [50.755724, 0.492369],
+  zoom: 5
+  });
