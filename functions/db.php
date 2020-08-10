40fc965db6a5acb5c81d4107cc1f95c2eb1c4218
Robin Geagea <Robin.geagea.rg@gmail.com>
2020-08-10 16:53:10 +0300

    database update

---
+++ functions/db.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_name="build_back_beirut";
+$user = 'root';
+$password = 'root';
+$host = 'localhost';
+$port = 8889;
+
+$conn = mysqli_connect(
+   $host,
+   $user,
+   $password,
+   $db_name,
+   $port
+);
+
+
+?>
